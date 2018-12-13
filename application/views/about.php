9ca7a4f4db58b4f07c8015c6969e5c4ccdc45c0b
nikendickna <nikendickna@gmail.com>
2018-12-13 17:59:05 +0700

    commit pertama

---
+++ application/views/about.php
@@ -0,0 +1,441 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>About us</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Travello template project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('styles/bootstrap4/bootstrap.min.css')?>">
+<link href="<?php echo base_url('assets/font-awesome-4.7.0/css/font-awesome.min.css')?>" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/OwlCarousel2-2.2.1/owl.carousel.css')?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/OwlCarousel2-2.2.1/owl.theme.default.css')?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/OwlCarousel2-2.2.1/animate.css')?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('styles/about.css')?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('styles/about_responsive.css')?>">
+</head>
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="header_content d-flex flex-row align-items-center justify-content-start">
+						<div class="header_content_inner d-flex flex-row align-items-end justify-content-start">
+							<div class="logo"><a href="./">JalanYuk!</a></div>
+							<nav class="main_nav">
+								<ul class="d-flex flex-row align-items-start justify-content-start">
+									<li><a href="./">Home</a></li>
+									<li class="active"><a href="./about">About us</a></li>
+									<li><a href="./destination">Destinations</a></li>
+									<li><a href="./news">News</a></li>
+									<li><a href="./contact">Contact</a></li>
+								</ul>
+							</nav>
+							<div class="header_phone ml-auto">Call us: 083 834 320 002</div>
+
+							<!-- Hamburger -->
+
+							<div class="hamburger ml-auto">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="header_social d-flex flex-row align-items-center justify-content-start">
+			<ul class="d-flex flex-row align-items-start justify-content-start">
+				<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+			</ul>
+		</div>
+	</header>
+
+	<!-- Menu -->
+
+	<div class="menu">
+		<div class="menu_header d-flex flex-row align-items-center justify-content-start">
+			<div class="menu_logo"><a href="./">JalanYuk!</a></div>
+			<div class="menu_close_container ml-auto"><div class="menu_close"><div></div><div></div></div></div>
+		</div>
+		<div class="menu_content">
+			<ul>
+				<li><a href="inex.html">Home</a></li>
+				<li><a href="./about">About us</a></li>
+				<li><a href="./destination">Destinations</a></li>
+				<li><a href="./news">News</a></li>
+				<li><a href="./contact">Contact</a></li>
+			</ul>
+		</div>
+		<div class="menu_social">
+			<div class="menu_phone ml-auto">Call us: 083 834 320 002</div>
+			<ul class="d-flex flex-row align-items-start justify-content-start">
+				<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<!-- Home -->
+
+	<div class="home">
+		<div class="background_image" style="background-image:url(images/new.jpg)"></div>
+	</div>
+
+	<!-- Search -->
+
+	<div class="home_search">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="home_search_container">
+						<div class="home_search_title">Search for your trip</div>
+						<div class="home_search_content">
+							<form action="#" class="home_search_form" id="home_search_form">
+								<div class="d-flex flex-lg-row flex-column align-items-start justify-content-lg-between justify-content-start">
+									<input type="text" class="search_input search_input_1" placeholder="City" required="required">
+									<input type="text" class="search_input search_input_2" placeholder="Departure" required="required">
+									<input type="text" class="search_input search_input_3" placeholder="Arrival" required="required">
+									<input type="text" class="search_input search_input_4" placeholder="Budget" required="required">
+									<button class="home_search_button">search</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- About -->
+
+	<div class="about">
+		<div class="container">
+			<div class="row">
+				<div class="col text-center">
+					<div class="section_subtitle">JalanYuk!</div>
+					<div class="section_title"><h2>A few words about us</h2></div>
+				</div>
+			</div>
+			<div class="row about_row">
+				<div class="col-lg-6">
+					<div class="about_content">
+						<div class="text_highlight">Apasih JalanYuk! itu?</div>
+						<div class="about_text">
+							<p>JalanYuk! adalah Travel & Trip Organizer yang berdiri dibawah nauangan PT. Pakar Perancang Liburan Murah Menyenangkan (PPLMM). JalanYuk! berdomisili di Kota Batu Jawa Timur.
+								Kami menyediakan berbagai paket yang telah didesain untuk mengakomodir kebutuhan trip masyarakat dari berbagai kalangan serta untuk wisatawan lokal dan wisatawan asing.
+								Paket tersedia dalam paket private trip dan open trip untuk destinasi wisata di area Jawa Timur, Madura, Bali, Yogyakarta dan Lombok.
+								Memiliki visi untuk terus membagi kesenangan kepada para jalaners, kami menyediakan trip yang aman dan nyaman dengan kru-kru yang berpengalaman.</p>
+						</div>
+						<div class="button about_button"><a href="#">read more</a></div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="about_image"><img src="images/about_1.jpg" alt=""></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Milestones -->
+
+	<div class="milestones">
+		<div class="container">
+			<div class="row">
+				
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="images/mountain.svg" alt=""></div>
+						<div class="milestone_counter" data-end-value="17">0</div>
+						<div class="milestone_text">Online Courses</div>
+					</div>
+				</div>
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="images/island.svg" alt=""></div>
+						<div class="milestone_counter" data-end-value="213">0</div>
+						<div class="milestone_text">Students</div>
+					</div>
+				</div>
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="images/camera.svg" alt=""></div>
+						<div class="milestone_counter" data-end-value="11923">0</div>
+						<div class="milestone_text">Teachers</div>
+					</div>
+				</div>
+
+				<!-- Milestone -->
+				<div class="col-lg-3 milestone_col">
+					<div class="milestone text-center">
+						<div class="milestone_icon"><img src="images/boat.svg" alt=""></div>
+						<div class="milestone_counter" data-end-value="15">0</div>
+						<div class="milestone_text">Countries</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Why Choose Us -->
+
+	<div class="why">
+		<div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/why.jpg" data-speed="0.8"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col text-center">
+					<div class="section_subtitle">JalanYuk!</div>
+					<div class="section_title"><h2>Why choose us?</h2></div>
+				</div>
+			</div>
+			<div class="row why_row">
+				
+				<!-- Why item -->
+				<div class="col-lg-4 why_col">
+					<div class="why_item">
+						<div class="why_image">
+							<img src="images/why_1.jpg" alt="">
+							<div class="why_icon d-flex flex-column align-items-center justify-content-center">
+								<img src="images/why_1.svg" alt="">
+							</div>
+						</div>
+						<div class="why_content text-center">
+							<div class="why_title">Fast Services</div>
+							<div class="why_text">
+								<p>Pernah gak si kalian uda ngebet banget pengen liburan tapi trip travel yang kalian pilih slow respon?
+									Tenang JalanYuk mengutamakan kesenangan jalaners dengan fast respon chat kami.
+									Tak perlu ragu untuk bertanya lebih dahulu karena tetap kita balas dengan cepat.
+									Tak hanya pesan kalian yang sigap kita tanggapi namun pelayanan kamipun tak kalah sigap dan cepat.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Why item -->
+				<div class="col-lg-4 why_col">
+					<div class="why_item">
+						<div class="why_image">
+							<img src="images/why_2.jpg" alt="">
+							<div class="why_icon d-flex flex-column align-items-center justify-content-center">
+								<img src="images/why_2.svg" alt="">
+							</div>
+						</div>
+						<div class="why_content text-center">
+							<div class="why_title">Great Team</div>
+							<div class="why_text">
+								<p>Walaupun kami belum lama bergabung dalam trip travel ini namun kami terus bertumbuh dan berekspansi untuk dapat menyediakan fasilitas trip yang menyenangkan.
+								Seluruh kru yang dipilih untuk menemani perjalanan wisata jalaners telah memenuhi kompetensi sehingga mampu memberikan kenyamanan selama perjalanan trip.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Why item -->
+				<div class="col-lg-4 why_col">
+					<div class="why_item">
+						<div class="why_image">
+							<img src="images/why_3.jpg" alt="">
+							<div class="why_icon d-flex flex-column align-items-center justify-content-center">
+								<img src="images/why_3.svg" alt="">
+							</div>
+						</div>
+						<div class="why_content text-center">
+							<div class="why_title">Best Deals</div>
+							<div class="why_text">
+								<p>Pernah dapat open trip murah namun mengecewakan?
+									Tenang JalanYuk! menawarkan harga murah namun tak mengesampingkan pelayanan dan perjalanan 
+									agar jalaners tetap senang dan nyaman bersama kami.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Team -->
+
+	<div class="team">
+		<div class="container">
+			<div class="row">
+				<div class="col text-center">
+					<div class="section_subtitle">JalanYuk!</div>
+					<div class="section_title"><h2>Meet the Team</h2></div>
+				</div>
+			</div>
+			<div class="row team_row">
+				
+				<!-- Team Item -->
+				<div class="col-xl-3 col-md-6 team_col">
+					<div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
+						<div class="team_image"><img src="images/dickna3.jpeg" alt=""></div>
+						<div class="team_content">
+							<div class="team_title"><a href="#">Dickna Niken</a></div>
+							<div class="team_text">
+								<p>You can found me on instagram @Ndickna</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Team Item -->
+				<div class="col-xl-3 col-md-6 team_col">
+					<div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
+						<div class="team_image"><img src="images/yusril.jpg" alt=""></div>
+						<div class="team_content">
+							<div class="team_title"><a href="#">Yusril Adek</a></div>
+							<div class="team_text">
+								<p>You can found me on instagram @phituzwarrasss</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Team Item -->
+				<div class="col-xl-3 col-md-6 team_col">
+					<div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
+						<div class="team_image"><img src="images/hardian.jpeg" alt=""></div>
+						<div class="team_content">
+							<div class="team_title"><a href="#">Hardian Rachmadi</a></div>
+							<div class="team_text">
+								<p>You can found me on instagram @Hardian_rachmadi</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Team Item -->
+				<div class="col-xl-3 col-md-6 team_col">
+					<div class="team_item d-flex flex-column align-items-center justify-content-start text-center">
+						<div class="team_image"><img src="images/andi2.jpeg" alt=""></div>
+						<div class="team_content">
+							<div class="team_title"><a href="#">Andy Surya</a></div>
+							<div class="team_text">
+								<p>You can found me on instagram @andy.s_f</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+
+	<footer class="footer">
+		<div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/footer_1.jpg" data-speed="0.8"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="newsletter">
+						<div class="newsletter_title_container text-center">
+							<div class="newsletter_title">Subscribe to our newsletter to get the latest trends & news</div>
+							<div class="newsletter_subtitle">Join our database NOW!</div>
+						</div>
+						<div class="newsletter_form_container">
+							<form action="#" class="newsletter_form d-flex flex-md-row flex-column align-items-start justify-content-between" id="newsletter_form">
+								<div class="d-flex flex-md-row flex-column align-items-start justify-content-between">
+									<div><input type="text" class="newsletter_input newsletter_input_name" id="newsletter_input_name" placeholder="Name" required="required"><div class="input_border"></div></div>
+									<div><input type="email" class="newsletter_input newsletter_input_email" id="newsletter_input_email" placeholder="Your e-mail" required="required"><div class="input_border"></div></div>
+								</div>
+								<div><button class="newsletter_button">subscribe</button></div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row footer_contact_row">
+				<div class="col-xl-10 offset-xl-1">
+					<div class="row">
+
+						<!-- Footer Contact Item -->
+						<div class="col-xl-4 footer_contact_col">
+							<div class="footer_contact_item d-flex flex-column align-items-center justify-content-start text-center">
+								<div class="footer_contact_icon"><img src="images/sign.svg" alt=""></div>
+								<div class="footer_contact_title">give us a call</div>
+								<div class="footer_contact_list">
+									<ul>
+										<li>Office Landline: 081 555 646 793</li>
+										<li>Mobile: 083 834 420 002</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- Footer Contact Item -->
+						<div class="col-xl-4 footer_contact_col">
+							<div class="footer_contact_item d-flex flex-column align-items-center justify-content-start text-center">
+								<div class="footer_contact_icon"><img src="images/trekking.svg" alt=""></div>
+								<div class="footer_contact_title">come & drop by</div>
+								<div class="footer_contact_list">
+									<ul style="max-width:190px">
+										<li>Perum Puri Indah A2/6 Beji-Batu</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+						<!-- Footer Contact Item -->
+						<div class="col-xl-4 footer_contact_col">
+							<div class="footer_contact_item d-flex flex-column align-items-center justify-content-start text-center">
+								<div class="footer_contact_icon"><img src="images/around.svg" alt=""></div>
+								<div class="footer_contact_title">send us a message</div>
+								<div class="footer_contact_list">
+									<ul>
+										<li>nikendickna@gmail.com</li>
+										<li>Office@JalanYuk.com</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col text-center"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Ndickna</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> </div>
+
+	</footer>
+</div>
+
+<script src="<?php echo base_url('js/jquery-3.2.1.min.js')?>"></script>
+<script src="<?php echo base_url('styles/bootstrap4/popper.js')?>"></script>
+<script src="<?php echo base_url('styles/bootstrap4/bootstrap.min.js')?>"></script>
+<script src="<?php echo base_url('assets/greensock/TweenMax.min.js')?>"></script>
+<script src="<?php echo base_url('assets/greensock/TimelineMax.min.js')?>"></script>
+<script src="<?php echo base_url('assets/scrollmagic/ScrollMagic.min.')?>"></script>
+<script src="<?php echo base_url('assets/greensock/animation.gsap.min.js')?>"></script>
+<script src="<?php echo base_url('assets/greensock/ScrollToPlugin.min.js')?>"></script>
+<script src="<?php echo base_url('assets/OwlCarousel2-2.2.1/owl.carousel.js')?>"></script>
+<script src="<?php echo base_url('assets/easing/easing.js')?>"></script>
+<script src="<?php echo base_url('assets/parallax-js-master/parallax.min.js')?>"></script>
+<script src="<?php echo base_url('js/about.js')?>"></script>
+</body>
+</html>
